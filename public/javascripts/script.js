ddd45a455c51588e72faed15310f9c95c74e1b06
Nate Wilson <46329080+natekwilson@users.noreply.github.com>
2019-11-06 15:56:32 -0700

    copy from last lab

---
+++ public/javascripts/script.js
@@ -0,0 +1,28 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    cities: [],
+    prefix: '',
+  },
+  methods: {
+    fetchREST() {
+      console.log("In Fetch " + this.prefix);
+      var url = "http://cs260labs.natewilsonit.com:3000/getcity?q=" + this.prefix;
+      console.log("URL " + url);
+      fetch(url)
+        .then((data) => {
+          return (data.json());
+        })
+        .then((citylist) => {
+          console.log("CityList");
+          console.log(citylist);
+          this.cities = [];
+          for (let i = 0; i < citylist.length; i++) {
+            console.log(citylist[i].city);
+            this.cities.push({ name: citylist[i].city });
+          };
+          console.log("Got Citylist");
+        });
+    },
+  },
+});
