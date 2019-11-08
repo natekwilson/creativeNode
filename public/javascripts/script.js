991071c29f7a586c350152e458c99758a7389697
Nate Wilson <46329080+natekwilson@users.noreply.github.com>
2019-11-08 07:05:56 -0700

    its a mess

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,93 @@
+
+
 var app = new Vue({
   el: '#app',
   data: {
-    cities: [],
-    prefix: '',
+      someOtherProperty: null,
+      ddTestVm: {
+          originalValue: [],
+          ddTestSelectedOption: "Value1",
+          disabled: false,
+          readonly: false,
+          visible: true,
+          color: "",
+          options: [
+              {
+                  "value": "Access",
+                  "text": "Access"
+              },
+              {
+                  "value": "Akai",
+                  "text": "Akai"
+              },
+              {
+                  "value": "Alesis",
+                  "text": "Alesis"
+              },
+              {
+                  "value": "Arturia",
+                  "text": "Arturia"
+              },
+              {
+                  "value": "Casio",
+                  "text": "Casio"
+              },
+              {
+                  "value": "Dreadbox",
+                  "text": "Dreadbox"
+              },
+              {
+                  "value": "Korg",
+                  "text": "Korg"
+              },
+              {
+                  "value": "Make Noise",
+                  "text": "Make Noise"
+              },
+              {
+                  "value": "Maker",
+                  "text": "Maker"
+              },
+              {
+                  "value": "Moog",
+                  "text": "Moog"
+              },
+              {
+                  "value": "Novation",
+                  "text": "Novation"
+              },
+              {
+                  "value": "Oberheim",
+                  "text": "Oberheim"
+              },
+              {
+                  "value": "Pittsburgh Modular",
+                  "text": "Pittsburgh Modular"
+              },
+              {
+                  "value": "Roland",
+                  "text": "Roland"
+              },
+              {
+                  "value": "Sequential Circuits",
+                  "text": "Sequential Circuits"
+              },
+              {
+                  "value": "Teenage Engineering",
+                  "text": "Teenage Engineering"
+              },
+              {
+                  "value": "Yamaha",
+                  "text": "Yamaha"
+              }
+          ]
+      },
+      synths: [],
   },
   methods: {
-    fetchREST() {
+    /*fetchREST() {
       console.log("In Fetch " + this.prefix);
-      var url = "http://cs260labs.natewilsonit.com:3000/getcity?q=" + this.prefix;
+      var url = "http://cs260labs.natewilsonit.com:3000/getsynth?q=" + this.prefix;
       console.log("URL " + url);
       fetch(url)
         .then((data) => {
@@ -23,6 +103,24 @@ var app = new Vue({
           };
           console.log("Got Citylist");
         });
-    },
+    },*/
+    changeItem: async function () 
+    {
+      //grab some remote data
+          try {
+            console.log("In Fetch ");
+              let response = await this.$http.get('http://cs260.natewilsonit.com:3000/getsynth?q=' + this.ddTestVm.ddTestSelectedOption + '.json');
+              console.log(response.data);
+              this.synths = response.data;
+          } catch (error) {
+              console.log(error)
+          }
+    }
   },
+  watch: {
+
+  },
+  async created() {
+
+  }
 });
